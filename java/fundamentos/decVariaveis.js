a29643dbdced2c223c6902d74fc169d699aed097
Iann Gabriel <iann.domingos@gmail.com>
2025-07-14 16:44:28 -0300

    Organizando arquivo e testando variaveis

--- java/fundamentos/decVariaveis.js
+++ java/fundamentos/decVariaveis.js
@@ -1,3 +1,12 @@
+// Declarar variáveis
+let nome = 'Iann';
+const sobrenome = 'Gabriel Domingos';
+
+//nome = nome & ' ' & sobrenome;
+
+// Exibir no console
+console.log(nome);
+
 
 const precoCombustivel = 5.79;
 const kmporlitro = 0.7
