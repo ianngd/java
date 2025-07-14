198b7f89b672e8713a03f91012e85efbca3dcab7
Iann Gabriel <iann.domingos@gmail.com>
2025-07-14 16:16:07 -0300

    Estudando fundamentos do java

---
+++ java/fundamentos/decVariaveis.js
@@ -0,0 +1,17 @@
+// Imprimir coisas no terminal
+console.log("E aee Iann. Suave?");
+
+// Declaração de variaveis
+// Let é variavel e posso mudar o valor depois
+let casa = "sofá";
+// Const é constante e não posso mudar o valor depois
+const nome = "Iann";
+
+
+console.log(nome);
+
+let preco_combustivel = 5.79;
+let combustivel_pkm = 0.7
+let distancia_km = 300;
+let gasto_viagem = (distancia_km/ combustivel_pkm) * preco_combustivel;
+console.log (gasto_viagem);
