9cc2064883f393cd61e3c57f87ad36126543d746
Iann Gabriel <iann.domingos@gmail.com>
2025-07-14 16:39:36 -0300

    Merge branch 'main' of https://github.com/ianngd/java

--- java/fundamentos/decVariaveis.js
+++ java/fundamentos/decVariaveis.js
@@ -1,17 +1,8 @@
-// Imprimir coisas no terminal
-console.log("E aee Iann. Suave?");
 
-// Declaração de variaveis
-// Let é variavel e posso mudar o valor depois
-let casa = "sofá";
-// Const é constante e não posso mudar o valor depois
-const nome = "Iann";
+const precoCombustivel = 5.79;
+const kmporlitro = 0.7
+const distanciaKm = 300;
 
+const gastoViagem = (distancia_km/ combustivel_pkm) * preco_combustivel;
 
-console.log(nome);
-
-let preco_combustivel = 5.79;
-let combustivel_pkm = 0.7
-let distancia_km = 300;
-let gasto_viagem = (distancia_km/ combustivel_pkm) * preco_combustivel;
 console.log (gasto_viagem);
